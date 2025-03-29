47e1c91562ea09e56a452d73aa493b8ebd42ad43
LeandroLabanca <163004478+LeandroLabanca@users.noreply.github.com>
2025-03-29 17:32:57 +0100

    Create proteinbiosynthesis.py

---
+++ .github/workflows/proteinbiosynthesis.py
@@ -0,0 +1,31 @@
+from Bio.Seq import Seq
+
+path = "C:/Users/leand/Downloads/"
+filename = "1a80.txt"
+
+file = open(path  + filename, "r")
+gene = file.read()
+
+gene = gene.replace ("\n", "")
+gene = gene.replace("\r", "")
+
+coding_DNA = Seq(gene)
+
+mRNA = coding_DNA.transcribe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(gene)
+print(mRNA)
