80cd1a9c8918fc551daed7f77d21829c47e028f5
Leandro labanca <leandro.labanca@bluewin.ch>
2025-07-08 12:23:36 +0200

    -

--- .github/workflows/proteinbiosynthesis.py
+++ .github/workflows/proteinbiosynthesis.py
@@ -1,26 +1,32 @@
 from Bio.Seq import Seq
 
-path = "C:/Users/leand/Downloads/"
-filename = "1a80.txt"
-
-file = open(path  + filename, "r")
-gene = file.read()
-
-gene = gene.replace ("\n", "")
-gene = gene.replace("\r", "")
-
-coding_DNA = Seq(gene)
-
-mRNA = coding_DNA.transcribe()
 
 
+def read():
+    path = "C:/Users/leand/Downloads/"
+    filename = "1a80.txt"
+    file = open(path  + filename, "r")
+    gene = file.read()
+    #removing newlines \n and removing carriage return \r for formatting
+    gene = gene.replace ("\n", "").replace("\r", "")
 
+def coding_DNA_sequence_biosynthesis(gene):
+    coding_dna = Seq(gene)
+    mrna = coding_dna.transcribe()
+    amino_acids = mrna.translate()
+    return(mrna)
+    return(amino_acids)
+def genomic_DNA():
+    pass
 
-#test
+read()
 
-
-
-
-
-print(gene)
-print(mRNA)
+if input == "CDS":
+    coding_DNA_sequence_biosynthesis()
+elif input == "genomic DNA without introns":
+    genomic_DNA()
+elif input == "genomic DNA with known introns":
+    pass
+else:
+    print("An approach to typical intron structures will be made and they will be removed")
+    pass
