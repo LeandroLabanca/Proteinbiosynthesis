a504663f4088eead428e5fa887342c67253f26bf
Leandro labanca <leandro.labanca@bluewin.ch>
2025-07-14 11:46:17 +0200

    -

--- .github/workflows/GUI.py
+++ .github/workflows/GUI.py
@@ -1,32 +1,27 @@
 import sys
-from PyQt5.QtCore import QSize,Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QAction, QApplication, QLabel, QMainWindow, QMenu
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("ProteinBioSynthesis")
-        button = QPushButton("Press me")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_clicked)
-        button.clicked.connect(self.button_toggled)
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
 
-        self.setCentralWidget(button)
         self.setMinimumSize(1920, 1080)
         self.setMaximumSize(3840, 2160)
 
-    def button_clicked(self):
-        print("Button clicked")
 
-    def button_toggled(self, checked):
-        print("Button toggled", checked)
 
 
 
 
 app=QApplication(sys.argv)
-
-window=MainWindow()
+window = MainWindow()
 window.show()
 app.exec_()
